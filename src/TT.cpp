6ed3f884a914b9819d6696b6c272bf0a4521f151
zEclipseVM <60575976+nbgsmk@users.noreply.github.com>
2024-06-28 00:56:57 +0200

    Initial

---
+++ src/TT.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : TT.cpp
+// Author      : z
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hey" << endl; // prints Hey
+	return 0;
+}
